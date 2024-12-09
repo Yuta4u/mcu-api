ff6fb3fba5b6a809ffb8b646083a2896e91c8af8
Yuta4u <yudhistira4u@gmail.com>
2024-12-10 00:18:12 +0700

    config:db

--- config/db.config.js
+++ config/db.config.js
@@ -1,34 +1,27 @@
 const pg = require("pg")
 
-// "username": "postgres.prosenwwbxwgokvlywtq",
-// "password": "Tomzriderx02",
-// "database": "postgres",
-// "host": "aws-0-ap-southeast-1.pooler.supabase.com",
-// "dialect": "postgres"
-
-// DEVELOPMENT
 const config = {
   development: {
-    username: "postgres.prosenwwbxwgokvlywtq",
-    password: "Tomzriderx02",
-    database: "postgres",
-    host: "aws-0-ap-southeast-1.pooler.supabase.com",
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: "postgres",
     dialecModule: pg,
   },
   test: {
-    username: "postgres.prosenwwbxwgokvlywtq",
-    password: "Tomzriderx02",
-    database: "postgres",
-    host: "aws-0-ap-southeast-1.pooler.supabase.com",
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: "postgres",
     dialecModule: pg,
   },
   production: {
-    username: "postgres.prosenwwbxwgokvlywtq",
-    password: "Tomzriderx02",
-    database: "postgres",
-    host: "aws-0-ap-southeast-1.pooler.supabase.com",
+    username: process.env.USERNAME,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+    host: process.env.HOST,
     dialect: "postgres",
     dialecModule: pg,
   },
